dd917ffea3912c76fa20d77330c8c4676a4ab771
Gabriel <segaz2002@gmail.com>
2018-02-02 01:33:05 +0200

    Jew MN setup

---
+++ jew_masternode_setup.sh
@@ -0,0 +1,35 @@
+sudo apt-get update -y
+apt-get upgrade -y
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev libboost-all-dev autoconf automake
+sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
+sudo apt-get install git
+git clone https://github.com/shekeltechnologies/JewNew.git
+add-apt-repository ppa:bitcoin/bitcoin
+apt-get install software-properties-common
+apt-get install libdb-dev libdb++-dev
+
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+
+cd JewNew
+chmod +x autogen.sh
+cd share
+chmod +x genbuild.sh
+cd ..
+cd src
+cd leveldb
+chmod +x build_detect_platform
+cd ..
+cd ..
+./autogen.sh
+./configure --with-incompatible-bdb
+make
+make install
+
+echo rpcuser=shekelrpc >> /root/.shekel/shekel.conf
+echo rpcpassword=3zoMLbdN6SLayKahEhV43NVcpfVJZb3dWWEW9QdkMdZZ >> /root/.shekel/shekel.conf
+shekeld -daemon
